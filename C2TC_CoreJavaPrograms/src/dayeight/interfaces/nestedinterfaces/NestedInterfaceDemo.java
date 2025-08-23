08e1b641da78a2f31a6381b6d808ca3c3f25e026
Padma Shree <150917358+PadmashreeArunprasath@users.noreply.github.com>
2025-08-23 21:34:43 +0530

    commit

--- C2TC_CoreJavaPrograms/src/dayeight/interfaces/nestedinterfaces/NestedInterfaceDemo.java
+++ C2TC_CoreJavaPrograms/src/dayeight/interfaces/nestedinterfaces/NestedInterfaceDemo.java
@@ -2,13 +2,10 @@
 package dayeight.interfaces.nestedinterfaces;
 
 public class NestedInterfaceDemo {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		NestedInterfaceClass obj = new NestedInterfaceClass();
 		obj.print();
 		System.out.println(NestedInterfaceClass.id);
-
 	}
-
 }
