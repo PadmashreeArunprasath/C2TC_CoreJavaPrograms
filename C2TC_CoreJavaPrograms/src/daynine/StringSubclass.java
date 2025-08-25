8eb5770bb4d754e199859069a0101e1f584b9de1
Padma Shree <150917358+PadmashreeArunprasath@users.noreply.github.com>
2025-08-25 19:19:13 +0530

    commit

--- C2TC_CoreJavaPrograms/src/daynine/StringSubclass.java
+++ C2TC_CoreJavaPrograms/src/daynine/StringSubclass.java
@@ -2,11 +2,11 @@
 
 //can't inherit final classes
 package daynine;
-
-public class StringSubClass extends String {
-
-	public static void main(String[] args) {
-		
-
-	}
-}
+//
+//public class StringSubClass extends String {
+//
+//	public static void main(String[] args) {
+//		
+//
+//	}
+//}
